ed56c980d3710266e5ba541c25e9509fb7d853a9
Gabriel DE ALMEIDA <gabriel-da@mbp-de-gabriel.home>
2021-11-30 15:11:43 +0100

    adding command user create tests

--- tests/feature/AddNewUserCommandTest.php
+++ tests/feature/AddNewUserCommandTest.php
@@ -2,9 +2,11 @@
 
 namespace App\Tests\feature;
 
+use App\Entity\User;
 use App\Tests\DatabasePrimer;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class AddNewUserCommandTest extends KernelTestCase
 {
@@ -29,5 +31,24 @@ class AddNewUserCommandTest extends KernelTestCase
     public function user_creation_command_behaves_correctly()
     {
         $application = new Application(self::$kernel);
+
+        $command = $application->find('app:create-new-user');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([
+            'email'     => 'tata@gmail.com',
+            'password'  => 'password',
+            'roles'     => ['ROLE_ADMIN', 'ROLE_TOTO']
+        ]);
+
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $userRecord = $userRepository->findOneBy(['email' => 'tata@gmail.com']);
+        
+        $this->assertEquals('tata@gmail.com', $userRecord->getEmail());
+        $this->assertEquals('tata@gmail.com', $userRecord->getUsername());
+        $this->assertEquals('tata@gmail.com', $userRecord->getUserIdentifier());
+        $this->assertTrue(in_array('ROLE_ADMIN', $userRecord->getRoles()));
+        $this->assertTrue(in_array('ROLE_TOTO', $userRecord->getRoles()));
+        $this->assertTrue(in_array('ROLE_USER', $userRecord->getRoles()));
     }
 }
